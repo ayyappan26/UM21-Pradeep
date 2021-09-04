128ccaef1be64a4b55ae413c87c68f0e7086b436
Acer <Acer@Acer-lap>
2021-09-04 07:56:14 +0530

    first commit

---
+++ ApachePoi/src/com/ultramain/um21/basic34/Apache.java
@@ -0,0 +1,47 @@
+package com.ultramain.um21.basic34;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.*;
+
+
+public class Apache {
+
+	   public static void main(String [] args) throws IOException
+	   {
+		   String file="E:\\Um 21\\ApachePoi\\Apache\\Book.xlsx";
+		   
+		   FileInputStream fileinput=new FileInputStream(file);
+		   
+		   XSSFWorkbook workbook=new XSSFWorkbook(fileinput);
+		   
+		   XSSFSheet sheet=workbook.getSheet("Sheet1");
+		   
+	  
+	      int row=sheet.getLastRowNum();
+	      int col=sheet.getRow(1).getLastCellNum();
+	      
+	      for(int r=0;r<row;r++)
+	      {
+	    	  XSSFRow rows=sheet.getRow(r);
+	      for(int c=0;c<col;c++)
+	      {
+	    	    XSSFCell cols=rows.getCell(c);
+	        
+	    	    switch(cols.getCellType())
+	    	    {
+	    	    case STRING:System.out.print("   "  +cols.getStringCellValue());
+	    	            break;
+	    	    case NUMERIC:System.out.print("   "  +cols.getNumericCellValue());
+	    	            break;
+	    	    case BOOLEAN:System.out.print("   "  +cols.getBooleanCellValue());
+	    	            break;
+	    	    
+	    	    }
+	         }
+	       System.out.println();
+	}
+	}
+	}
+
+
